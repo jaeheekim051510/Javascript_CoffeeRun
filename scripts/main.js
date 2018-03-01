c4af78e5773f3b1ef134aefc5d1787bea87a32ad
Jaehee Kim <jaeheekim@Jaehees-MacBook-Pro.local>
2018-03-01 15:27:00 -0500

    remove local storage code and add code to load data using ajex

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,5 @@
 
 
-// var URL = 'http://dc-coffeerun.herokuapp.com/api/coffeeorders'
-// $.get(URL, function(data) {
-//     console.log
-// })
 
 // CREATE THE VARIABLES FOR THE DOM ELEMENTS WE NEED TO WORK WITH
 var coffeeForm = document.querySelector("[data-coffee-order='form']");
@@ -13,37 +9,46 @@ var size = document.querySelector("[name='size']");
 var flavor = document.querySelector("[name='flavor']");
 var strength = document.querySelector("[name='strength']");
 
+
+
 // Listing orders on page
 var coffeeList = document.querySelector('.coffee-list');
 var NewCoffeeList = function(order) {
     var list = document.createElement('li');
-    var orderText = order.coffee + ' ' + order.email + ' ' + order.size + ' ' + order.flavor + ' ' + order.strength
+    var orderText = 'coffee:'+ ' ' +  order.coffee + ', ' + 'email:'+ ' ' + order.emailAddress + ', ' + 'size:'+ ' ' + order.size + ', ' + 'flavor:'+ ' ' + order.flavor + ', ' + 'strength:'+ ' ' + order.strength
     list.textContent = orderText 
     coffeeList.appendChild(list);
-
+    
     //removing from the list
     list.addEventListener('click', function(event){
         list.remove();
-    })
+    });
 }
 
-//Local Storage 
-var orders = JSON.parse(localStorage.getItem('coffee')) || [];
-orders.forEach(function(order) {
-    NewCoffeeList(order);
+//load data using ajex
+var URL = 'http://dc-coffeerun.herokuapp.com/api/coffeeorders'
+$.get(URL, function(data) {
+    var orders = Object.values(data);
+        orders.forEach(function(order) {
+        NewCoffeeList(order);
+    })
 })
+
+//Local Storage 
+// var orders = JSON.parse(localStorage.getItem('coffee')) || [];
+// orders.forEach(function(order) {
+//     NewCoffeeList(order);
+// })
 
 //creating list of orders
 coffeeForm.addEventListener('submit', function(event){
-    var listOrder = ({coffee:coffeeOrder.value, email: emailAddress.value, size: size.value, flavor: flavor.value, strength: strength.value});
+    var listOrder = ({coffee:coffeeOrder.value, emailAddress: emailAddress.value, size: size.value, flavor: flavor.value, strength: strength.value});
     event.preventDefault();
     NewCoffeeList(listOrder);
     orders.push(listOrder);
 
     localStorage.setItem('coffee', JSON.stringify(orders));
 });
-
-
 
 
 
