ff48e98e5a61a7a81dd6dc1171e077078d6cbc46
Jaehee Kim <jaeheekim@Jaehees-MacBook-Pro.local>
2018-03-05 12:14:00 -0500

    add code that turns the list to green when clicked and removes from list after 2 secs.

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,9 @@
 // CREATE THE VARIABLES FOR THE DOM ELEMENTS WE NEED TO WORK WITH
 var coffeeForm = document.querySelector("[data-coffee-order='form']");
+
 var coffeeOrder = document.querySelector("[name='coffee']");
 var emailAddress = document.querySelector("[name='emailAddress']");
-var size = document.querySelector("[name='size']");
+var size = document.querySelectorAll("[name='size']");
 var flavor = document.querySelector("[name='flavor']");
 var strength = document.querySelector("[name='strength']");
 
@@ -13,13 +14,17 @@ var coffeeList = document.querySelector('.coffee-list');
 var NewCoffeeList = function(order) {
     var list = document.createElement('li');
     var orderText = 'coffee:'+ ' ' +  order.coffee + ', ' + 'email:'+ ' ' + order.emailAddress + ', ' + 'size:'+ ' ' + order.size + ', ' + 'flavor:'+ ' ' + order.flavor + ', ' + 'strength:'+ ' ' + order.strength
-    list.textContent = orderText 
+    list.textContent = orderText;
     coffeeList.appendChild(list);
-    
-    //removing from the list and data
+
+    // Click on a order list to hide the current list item and remove from data.
     list.addEventListener('click', function(event){
-        list.remove();
-        $.ajax(URL + '/' + order.emailAddress, {method:'delete'})
+        list.style.backgroundColor = 'green';
+        //Removes the list after 2 sec.
+        setTimeout(function() {
+            list.remove();
+            $.ajax(URL + '/' + order.emailAddress, {method:'delete'})
+        }, 2000);
     });
 }
 
@@ -34,18 +39,7 @@ $.get(URL, function(data) {
 //creating list of orders
 coffeeForm.addEventListener('submit', function(event){
     event.preventDefault();
-    var listOrder = ({coffee:coffeeOrder.value, emailAddress: emailAddress.value, size: size.value, flavor: flavor.value, strength: strength.value});
+    var listOrder = ({coffee:coffeeOrder.value, emailAddress: emailAddress.value, size: form.size.value, flavor: flavor.value, strength: strength.value});
     $.post(URL, listOrder);
-    // $.post(URL, function(data){
-    //    data:listOrder 
-    // })
     NewCoffeeList(listOrder);
-    // orders.push(listOrder);
-    // localStorage.setItem('coffee', JSON.stringify(orders));
 });
-
-//Local Storage 
-// var orders = JSON.parse(localStorage.getItem('coffee')) || [];
-// orders.forEach(function(order) {
-//     NewCoffeeList(order);
-// })
